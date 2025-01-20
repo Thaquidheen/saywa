6e69bbb77debd525ea8e12d6b1f659e4431b4046
Thaquidheen M A <71427234+Thaquidheen@users.noreply.github.com>
2025-01-20 20:50:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,29 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(cors());
+app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"], // example
+      },
+    },
+  })
+);
+
+const webhookRouter = require("./routes/Payments");
+app.use(
+  "/webhook",
+  // Use raw body parser only for this route:
+  bodyParser.raw({ type: "application/json" }),
+  webhookRouter
+);
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(function (req, res, next) {
   const apiKey = process.env.APP_API_KEY;
   const apiKeyRequest = req.header("x-api-key");
@@ -46,50 +69,10 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(function (req, res, next) {
-  const apiKey = process.env.APP_API_KEY;
-  const apiKeyRequest = req.header("x-api-key");
 
-  // if (apiKey !== apiKeyRequest)
-  //   return res.status(401).send({
-  //     ok: false,
-  //     error: "Access Denied",
-  //   });
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
-app.use(helmet());
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"],
-      },
-    },
-  })
-);
 
-app.use(
-  express.json({
-    limit: "5mb",
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  })
-);
-app.use(express.urlencoded({ extended: true }));
 const authRoutes = require("./routes/Login");
 const login = require("./routes/Login");
 const users = require("./routes/Users");

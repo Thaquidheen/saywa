61d79a79020f6179e80c9cf8bb71c7fbf52a637c
Thaquidheen M A <71427234+Thaquidheen@users.noreply.github.com>
2025-01-20 21:45:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
@@ -28,29 +27,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(cors());
-app.use(helmet());
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"], // example
-      },
-    },
-  })
-);
-
-const webhookRouter = require("./routes/Payments");
-app.use(
-  "/webhook",
-  // Use raw body parser only for this route:
-  bodyParser.raw({ type: "application/json" }),
-  webhookRouter
-);
-
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
-
 app.use(function (req, res, next) {
   const apiKey = process.env.APP_API_KEY;
   const apiKeyRequest = req.header("x-api-key");
@@ -70,16 +46,56 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
+app.use(function (req, res, next) {
+  const apiKey = process.env.APP_API_KEY;
+  const apiKeyRequest = req.header("x-api-key");
 
+  // if (apiKey !== apiKeyRequest)
+  //   return res.status(401).send({
+  //     ok: false,
+  //     error: "Access Denied",
+  //   });
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
+app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"],
+      },
+    },
+  })
+);
 
+app.use(
+  express.json({
+    limit: "5mb",
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 const authRoutes = require("./routes/Login");
 const login = require("./routes/Login");
 const users = require("./routes/Users");
 
 const vehicles = require("./routes/Vehicles");
-
+const webhookRouter = require("./routes/Payments");
 const trips = require("./routes/Trips");
 const customers = require("./routes/Customers");
 // const payments = require("./routes/Payments");
@@ -89,10 +105,6 @@ const referal = require("./routes/NewReferal");
 const test = require("./routes/test");
 const PACKAGES = require("./routes/Packages");
 const TAX = require("./routes/Tax");
-
-
-
-
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
@@ -102,7 +114,7 @@ app.use("/api/users", users);
 app.use("/api/vehicles", vehicles);
 app.use("/api/trip", trips);
 app.use("/api/customers", customers);
-
+app.use("/", webhookRouter);
 // app.use("/api/payments", payments);
 app.use("/api/notifications", notifications);
 app.use("/api/upload", uploadRouter);
